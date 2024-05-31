88b681df7d57e507a02fd83e6127b765c74d4e7f
SK Amjad Ali <amjadali@SKs-Laptop.local>
2024-05-31 17:15:05 +0530

    video Enable

--- cypress/e2e/cypress.config.js
+++ cypress/e2e/cypress.config.js
@@ -1,11 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  video: true,
   e2e: {
-    "baseUrl" : "http://localhost:8080",
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    }
+    },
   },
 });
